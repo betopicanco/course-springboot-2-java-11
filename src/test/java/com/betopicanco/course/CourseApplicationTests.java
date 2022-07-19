58d3d003c8a3a53e5fddf8a539c29e62acce7fdb
betopicanco <betopicanco@gmail.com>
2022-07-19 11:41:34 -0300

    Project created

---
+++ src/test/java/com/betopicanco/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betopicanco.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
