e1d9ab65b80701922d41324ec81fef265161532e
betopicanco <betopicanco@gmail.com>
2022-07-19 22:00:52 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/betopicanco/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.betopicanco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betopicanco.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
