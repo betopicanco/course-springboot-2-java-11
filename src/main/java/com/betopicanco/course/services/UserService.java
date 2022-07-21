a7f6194f3fcc0abca03a461235f911cb66185240
betopicanco <betopicanco@gmail.com>
2022-07-21 10:58:59 -0300

    Exception habdling - findById

--- src/main/java/com/betopicanco/course/services/UserService.java
+++ src/main/java/com/betopicanco/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.betopicanco.course.entities.User;
 import com.betopicanco.course.repositories.UserRepository;
+import com.betopicanco.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
