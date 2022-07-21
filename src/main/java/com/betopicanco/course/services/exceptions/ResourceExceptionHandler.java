2a0d273823ba08fec5d3459647e201b85f1ce329
betopicanco <betopicanco@gmail.com>
2022-07-21 11:21:18 -0300

    Exception handling - delete

--- src/main/java/com/betopicanco/course/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/betopicanco/course/services/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.betopicanco.course.resources.exceptions.DatabaseException;
 import com.betopicanco.course.resources.exceptions.StandardError;
 
 @ControllerAdvice
@@ -22,7 +23,24 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(
 			Instant.now(), 
 			status.value(), 
-			error, e.getMessage(), 
+			error, 
+			e.getMessage(), 
+			request.getRequestURI()
+		);
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError err = new StandardError(
+			Instant.now(),
+			status.value(),
+			error, 
+			e.getMessage(),
 			request.getRequestURI()
 		);
 		
